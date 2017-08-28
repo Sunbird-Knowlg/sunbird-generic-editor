998cfe3f9c732a2241a9c4ffce0a39bfd06609db
sunil <sunils@ilimi.in>
2017-08-28 18:58:56 +0530

    Issue #2 feat: editor environment setup

--- app/scripts/genericeditor/genericeditor-config.js
+++ app/scripts/genericeditor/genericeditor-config.js
@@ -1,4 +1,5 @@
 org.ekstep.contenteditor.config = _.assign(org.ekstep.contenteditor.config, {
+	pluginRepo: '/plugins',
     plugins: [
         { "id": "org.ekstep.genericeditorheader", "ver": "1.0", "type": "plugin" },
         { "id": "org.ekstep.download", "ver": "1.0", "type": "plugin" }
