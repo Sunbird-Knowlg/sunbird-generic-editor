ae4e638736e63f4e4908891d493743bec1734337
Kartheek Palla <palla.kartheekreddy@gmail.com>
2017-09-20 17:41:59 +0530

    Issue #636 fix: edit meta popup not working in generic editor

--- app/scripts/genericeditor/genericeditor-config.js
+++ app/scripts/genericeditor/genericeditor-config.js
@@ -1,10 +1,6 @@
 org.ekstep.contenteditor.config = _.assign(org.ekstep.contenteditor.config, {
 	pluginRepo: '/plugins',
     plugins: [
-        { "id": "org.ekstep.genericeditorheader", "ver": "1.0", "type": "plugin" },        
-        { "id": "org.ekstep.genericeditorpreview", "ver": "1.0", "type": "plugin" },        
-        { "id": "org.ekstep.genericeditorsidebar", "ver": "1.0", "type": "plugin" },
-        { "id": "org.ekstep.uploadcontent", "ver": "1.0", "type": "plugin" },
-        { "id": "org.ekstep.contenteditorfunctions", "ver": "1.0", "type": "plugin" }
+        { "id": "org.ekstep.genericeditorheader", "ver": "1.0", "type": "plugin" }
     ]
 });
