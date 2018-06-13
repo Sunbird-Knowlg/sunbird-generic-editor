be25b8f445e81ee3e72a87010ce27d62e6a7e108
rajeevsathish <rajeev.sathish@tarento.com>
2018-06-13 23:38:02 +0530

    Issue #000 feat:Plugin Bumpup to handle external content

--- gulpfile.js
+++ gulpfile.js
@@ -198,7 +198,7 @@ gulp.task('build', ['minify','inject', 'replace', 'packageCorePlugins', 'zip']);
 var corePlugins = [
     "org.ekstep.conceptselector-1.1",
     "org.ekstep.assetbrowser-1.2",
-    "org.ekstep.uploadcontent-1.0",
+    "org.ekstep.uploadcontent-1.1",
     "org.ekstep.contenteditorfunctions-1.2"
 ]
 
