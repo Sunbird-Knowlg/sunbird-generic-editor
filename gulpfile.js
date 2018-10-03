4f314bef3d94798efc150c8cbeb120c818b3a43d
Venkateshwaran Selvaraj <venkateshwarans@ilimi.in>
2018-10-03 13:17:15 +0530

    Issue #SB-6760 fix: Travis and Codacy package and gulp update

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ var merge = require('merge-stream');
 var replace = require('gulp-string-replace');
 const zip = require('gulp-zip');
 var uglify = require('gulp-uglify');
+var git = require('gulp-git');
 var versionNumber = process.env.version_number;
 var buildNumber = process.env.build_number;
 
